420e1cbcd38eea66be27de20bbc63ec0001e200d
Carlos Covarrubias <ccovarrubias@gemsdev.mx>
2025-03-04 14:39:47 -0600

    feat: Enhance OpenAPI spec generation with query and path parameter support

--- main.go
+++ main.go
@@ -16,12 +16,22 @@ type OpenAPI struct {
 
 // GenerateOpenAPISpec generates a basic OpenAPI spec
 type Route struct {
-	Method  string
-	Path    string
-	Summary string
-	Request interface{}
-	Response interface{}
+	Method   string
+	Path     string
+	Summary  string
+	Body     interface{}   // Request body schema (if applicable)
+	Query    []ParamSchema // Query parameters
+	PathVars []ParamSchema // Path parameters
+	Response interface{}   // Response schema
 }
+
+type ParamSchema struct {
+	Name        string
+	Description string
+	Required    bool
+	Type        string
+}
+
 
 func GenerateOpenAPISpec(routes []Route) OpenAPI {
 	spec := OpenAPI{
@@ -37,15 +47,9 @@ func GenerateOpenAPISpec(routes []Route) OpenAPI {
 		if _, exists := spec.Paths[route.Path]; !exists {
 			spec.Paths[route.Path] = make(map[string]interface{})
 		}
-		spec.Paths[route.Path].(map[string]interface{})[route.Method] = map[string]interface{}{
+
+		operation := map[string]interface{}{
 			"summary": route.Summary,
-			"requestBody": map[string]interface{}{
-				"content": map[string]interface{}{
-					"application/json": map[string]interface{}{
-						"schema": route.Request,
-					},
-				},
-			},
 			"responses": map[string]interface{}{
 				"200": map[string]interface{}{
 					"description": "Success",
@@ -57,10 +61,55 @@ func GenerateOpenAPISpec(routes []Route) OpenAPI {
 				},
 			},
 		}
+
+		// Add query parameters
+		if len(route.Query) > 0 {
+			params := []map[string]interface{}{}
+			for _, param := range route.Query {
+				params = append(params, map[string]interface{}{
+					"name":        param.Name,
+					"in":          "query",
+					"required":    param.Required,
+					"schema":      map[string]string{"type": param.Type},
+					"description": param.Description,
+				})
+			}
+			operation["parameters"] = params
+		}
+
+		// Add path parameters
+		if len(route.PathVars) > 0 {
+			if operation["parameters"] == nil {
+				operation["parameters"] = []map[string]interface{}{}
+			}
+			for _, param := range route.PathVars {
+				operation["parameters"] = append(operation["parameters"].([]map[string]interface{}), map[string]interface{}{
+					"name":        param.Name,
+					"in":          "path",
+					"required":    param.Required,
+					"schema":      map[string]string{"type": param.Type},
+					"description": param.Description,
+				})
+			}
+		}
+
+		// Add request body if applicable
+		if route.Body != nil {
+			operation["requestBody"] = map[string]interface{}{
+				"content": map[string]interface{}{
+					"application/json": map[string]interface{}{
+						"schema": route.Body,
+					},
+				},
+			}
+		}
+
+		spec.Paths[route.Path].(map[string]interface{})[route.Method] = operation
 	}
 
 	return spec
 }
+
 
 // ServeOpenAPIDocs serves the OpenAPI JSON
 type OpenAPIServer struct {
@@ -76,6 +125,20 @@ func NewOpenAPIServer() *OpenAPIServer {
 	return &OpenAPIServer{}
 }
 
-func (o *OpenAPIServer) AddRoute(method, path, summary string, request interface{}, response interface{}) {
-	o.Routes = append(o.Routes, Route{Method: method, Path: path, Summary: summary, Request: request, Response: response})
+func (o *OpenAPIServer) AddRoute(method, path, summary string, body interface{}, queryParams []ParamSchema, pathParams []ParamSchema, response interface{}) {
+	// Automatically prevent requestBody for GET
+	if method == "GET" {
+		body = nil
+	}
+
+	o.Routes = append(o.Routes, Route{
+		Method:   method,
+		Path:     path,
+		Summary:  summary,
+		Body:     body,
+		Query:    queryParams,
+		PathVars: pathParams,
+		Response: response,
+	})
 }
+
